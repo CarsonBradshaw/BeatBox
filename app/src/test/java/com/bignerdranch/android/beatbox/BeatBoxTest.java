c96bab3ce480d3d34a46bec759f144ffba5e6566
CarsonBradshaw <carsonabradshaw@gmail.com>
2017-05-27 00:00:15 -0600

    Added BeatBox unit test setup. Discovered break in MVVM patttern with model depending on context from view.

---
+++ app/src/test/java/com/bignerdranch/android/beatbox/BeatBoxTest.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.beatbox;
+
+import android.content.res.AssetManager;
+import android.media.SoundPool;
+
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by Admin on 5/26/2017.
+ */
+
+public class BeatBoxTest {
+
+    private AssetManager mAssets;
+    private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
+    private BeatBox mSubject;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
